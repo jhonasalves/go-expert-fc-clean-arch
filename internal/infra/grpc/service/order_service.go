f42d3fd3179ab355ed44596c6b4d86cdda397f46
Jhonas Alves <jhonas.alvesm@gmail.com>
2024-09-17 21:23:58 -0300

    feat: add ListOrders com grpc

--- internal/infra/grpc/service/order_service.go
+++ internal/infra/grpc/service/order_service.go
@@ -10,6 +10,7 @@ import (
 type OrderService struct {
 	pb.UnimplementedOrderServiceServer
 	CreateOrderUseCase usecase.CreateOrderUseCase
+	ListOrdersUseCase  usecase.ListOrderUseCase
 }
 
 func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase) *OrderService {
@@ -34,4 +35,26 @@ func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderReques
 		Tax:        float32(output.Tax),
 		FinalPrice: float32(output.FinalPrice),
 	}, nil
+}
+
+func (s *OrderService) ListOrders(ctx context.Context, in *pb.Blank) (*pb.OrderList, error) {
+	orders, err := s.ListOrdersUseCase.Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	var ordersResponse []*pb.CreateOrderResponse
+
+	for _, order := range orders {
+		orderReponse := &pb.CreateOrderResponse{
+			Id:         order.ID,
+			Price:      float32(order.Price),
+			Tax:        float32(order.Tax),
+			FinalPrice: float32(order.FinalPrice),
+		}
+
+		ordersResponse = append(ordersResponse, orderReponse)
+	}
+
+	return &pb.OrderList{Orders: ordersResponse}, nil
 }
