d447163aaf95c2912ecefcb3e08a071d2e24cdef
Jhonas Alves <jhonas.alvesm@gmail.com>
2024-09-19 00:19:58 -0300

    fix: implementar metodo FindAll interface OrderRepositoryInterface

--- internal/infra/database/order_repository.go
+++ internal/infra/database/order_repository.go
@@ -26,8 +26,8 @@ func (r *OrderRepository) Save(order *entity.Order) error {
 	return nil
 }
 
-func FindAll(db *sql.DB) ([]entity.Order, error) {
-	rows, err := db.Query("SELECT id, price, tax, final_price FROM orders")
+func (r *OrderRepository) FindAll() ([]entity.Order, error) {
+	rows, err := r.Db.Query("SELECT id, price, tax, final_price FROM orders")
 	if err != nil {
 		return nil, err
 	}
@@ -36,20 +36,10 @@ func FindAll(db *sql.DB) ([]entity.Order, error) {
 	var orders []entity.Order
 	for rows.Next() {
 		var order entity.Order
-		err := rows.Scan(&order.ID, &order.Price, &order.Tax, &order.FinalPrice)
-		if err != nil {
+		if err := rows.Scan(&order.ID, &order.Price, &order.Tax, &order.FinalPrice); err != nil {
 			return nil, err
 		}
 		orders = append(orders, order)
 	}
 	return orders, nil
-}
-
-func (r *OrderRepository) GetTotal() (int, error) {
-	var total int
-	err := r.Db.QueryRow("Select count(*) from orders").Scan(&total)
-	if err != nil {
-		return 0, err
-	}
-	return total, nil
 }
